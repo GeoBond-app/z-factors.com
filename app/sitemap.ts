00a844f557a5dc6a8adeaa6386c4454e9ac8a266
horsekimchi <132565545+horsekimchi@users.noreply.github.com>
2026-06-08 10:11:58 -0700

    Update canonical sitemap URLs

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,15 +3,15 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://z-factors.com',
+      url: 'https://www.z-factors.com',
       lastModified: new Date(),
     },
     {
-      url: 'https://z-factors.com/archive',
+      url: 'https://www.z-factors.com/archive',
       lastModified: new Date(),
     },
     {
-      url: 'https://z-factors.com/analysis/TA-001',
+      url: 'https://www.z-factors.com/analysis/TA-001',
       lastModified: new Date(),
     },
   ];
